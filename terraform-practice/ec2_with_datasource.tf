6bde49b0ea1f50d55b8dd9f3d61d9a91c1be8cf4
TechCloudifyMe <141027817+TechCloudifyMe@users.noreply.github.com>
2023-11-20 21:54:30 +0530

    Update ec2_with_datasource.tf

--- terraform-practice/ec2_with_datasource.tf
+++ terraform-practice/ec2_with_datasource.tf
@@ -3,6 +3,6 @@ resource "aws_instance" "ec2demo" {
   count              = var.instance_count
   ami                = lookup(var.ami_id,var.region)
   instance_type      = var.instance_type
-  availability_zone  = element(var.availability_zones, count.index) # count index starts with 0
+  availability_zone  = data.aws_availability_zone.available.region
   key_name      = "awsdevops"
 }
