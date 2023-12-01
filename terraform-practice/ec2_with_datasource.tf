58cbc64283b00829c0b81f1e083d77aed7497372
TechCloudifyMe <141027817+TechCloudifyMe@users.noreply.github.com>
2023-12-01 19:15:43 +0530

    Update ec2_with_datasource.tf

--- terraform-practice/ec2_with_datasource.tf
+++ terraform-practice/ec2_with_datasource.tf
@@ -3,5 +3,6 @@ resource "aws_instance" "ec2demo" {
   count              = var.instance_count
   instance_type      = var.instance_type
   ami                = data.aws_ami.latest_amazon_linux.id
+  user_data          = "${file("app.sh")}"
   key_name           = "awsdevops"
 }
