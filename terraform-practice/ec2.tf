a47c4f98afc41d538f5f05b0bfc5840538cc5d58
Aparna-Bhuma <Aparna.Bhuma@ibm.com>
2023-12-26 00:16:11 +0530

    terraform-practice

--- terraform-practice/ec2.tf
+++
@@ -1,6 +0,0 @@
-# Resource Block
-resource "aws_instance" "ec2demo" {
-  ami           = "ami-0533f2ba8a1995cf9" # Amazon Linux in us-east-1, update as per your region
-  instance_type = "t2.micro"
-  key_name      = "awsdevops"
-}
--- terraform-practice/ec2_with_datasource.tf
+++
@@ -1,8 +0,0 @@
-# Resource Block
-resource "aws_instance" "ec2demo" {
-  count              = var.instance_count
-  instance_type      = var.instance_type
-  ami                = data.aws_ami.latest_amazon_linux.id
-  user_data          = "${file("app.sh")}"
-  key_name           = "awsdevops"
-}
